c777a6e4028b5d6cd7252515477ad4e9f01833f8
AtguiguGitUserB <atguigu_git_b@163.com>
2018-12-11 15:31:09 +0800

    update main method

--- test/src/main/java/TestGit.java
+++ test/src/main/java/TestGit.java
@@ -2,6 +2,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("111111");
         System.out.println("222222");
+        System.out.println("333333");
 
     }
 }
